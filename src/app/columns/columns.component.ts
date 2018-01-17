90da99978c83aea61aef4c87bd30ad8e477949c5
Marius <c_marian98@yahoo.com>
2018-01-17 21:31:09 +0200

    drop anywhere in column fixed, task sorting after droping

--- src/app/columns/columns.component.ts
+++ src/app/columns/columns.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataProviderService } from '../dataProvider.service'
-import { DragulaService } from 'dragula'
+import { DragulaService } from 'ng2-dragula/components/dragula.provider';
 
 @Component({
   selector: 'app-columns',
@@ -11,16 +11,45 @@ import { DragulaService } from 'dragula'
 export class ColumnsComponent implements OnInit {
 	service = new DataProviderService;	
 
-  constructor(dataService: DataProviderService) { 
-  	this.service = dataService;
+  constructor(dataService: DataProviderService, dragula: DragulaService) { 
+    this.service = dataService;
+
+    dragula.drop.subscribe((value) => {
+      this.onDrop(value);
+    });
   }
 
   columns = [];
+  dict = [];
 
   ngOnInit() {
   	//cer informatiile despre coloane
     this.columns = this.service.fillColumns();
+
+    //fac un dictionar cu numele coloanelor -> indexul lor
+    for (let i in this.columns)
+    {
+      this.dict[this.columns[i].name] = i;
+    }
+
+
     console.log(this.columns);
   }
+
+
+  onDrop(args){
+    //imi trebuie args[2]
+    //args[2] = coloana unde s-a facut drag
+    let id = args[2].id;
+    let idIndex = this.dict[id];
+
+    console.log(this.columns[idIndex]);
+
+    //sortez taskurile
+    this.columns[idIndex].tasks.sort((t1,t2) => {return t1.importance - t2.importance});
+
+  }
+
+
 
 }
